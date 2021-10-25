d3ec887fe8c402e9aec6a8e71e4778a5214ba43d
dearyeon <d_earyeon@naver.com>
2021-10-25 22:01:59 +0900

    문자열 내 마음대로 정렬하기

---
+++ level1/문자열_내_마음대로_정렬하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int idx;
+
+bool cmp (string a, string b) {
+    return a[idx] == b[idx] ? a < b : a[idx] < b[idx];
+} 
+
+vector<string> solution(vector<string> strings, int n) {
+    idx = n;
+    sort(strings.begin(), strings.end(), cmp);
+    return strings;
+}
