bef1e009155e92c40976546b14796f0b51f94fd1
dearyeon <d_earyeon@naver.com>
2021-10-25 21:57:21 +0900

    나누어 떨어지는 숫자 배열

---
+++ level1/나누어_떨어지는_숫자_배열.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+    for(int num:arr) 
+        if(num%divisor==0) answer.push_back(num);
+    sort(answer.begin(), answer.end());
+    return answer.empty() ? vector<int>(1,-1) : answer;
+}
