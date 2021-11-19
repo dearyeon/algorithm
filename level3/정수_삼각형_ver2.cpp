234e45c9445edbf333f8843cb78797a9361de131
dearyeon <d_earyeon@naver.com>
2021-11-19 17:13:45 +0900

    정수 삼각형

--- level3/정수_삼각형_ver2.cpp
+++ level3/정수_삼각형_ver2.cpp
@@ -7,9 +7,8 @@ int solution(vector<vector<int>> triangle) {
     vector<vector<int>> cnt = triangle;
 
     for(int i=cnt.size()-2;i>=0;i--) {
-        for(int j=0;j<cnt[i].size();j++) {
+        for(int j=0;j<cnt[i].size();j++)
             cnt[i][j] += max(cnt[i+1][j], cnt[i+1][j+1]);
-        }
     }
     
     return cnt[0][0];
