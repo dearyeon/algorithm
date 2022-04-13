c22bdf1ea3736b02f75da1ed0dfe244fe70dfe50
dearyeon <d_earyeon@naver.com>
2022-04-13 23:34:28 +0900

    방문길이

--- programmers_level2/방문_길이.cpp
+++ programmers_level2/방문_길이.cpp
@@ -8,10 +8,7 @@ int solution(string dirs) {
     int visit[21][11] = { 0 };
     int x, y;
     char buf;
-    // U - (9, 5)
-    // D - (11, 5)
-    // R - (10, 5)
-    // L - (10, 4)
+    
     if(dirs[0] == 'U') x = 9, y = 5, buf = 'U';
     else if(dirs[0] == 'D') x = 11, y = 5, buf = 'D';
     else if(dirs[0] == 'R') x = 10, y = 5, buf = 'R';
@@ -21,30 +18,30 @@ int solution(string dirs) {
     
     dirs.erase(0,1);
     
-    // U - D   D - U.   R - L.  L - R 같음
-    // U - R (1, 0) -> (0, 0)    "-1, 0"    U - L (1,1) -> (0,0)  "+1, +1"
-    // D - R (1,0) -> (2,0)    "+1, 0"  D - L (1,1) -> (2,0)  "+1, -1"
-    // R - U (2,0) -> (1,1)   "-1, +1"   R - D (2,0) -> (3,1)  "+1, +1"
-    // L - U (2,0) -> (1, 0)   "-1, 0"   L - D (2,0) -> (3,0)   "+1, 0"
-    cout << buf << " (" << x << "," << y << ") " << answer << endl;
-    
     for(char c : dirs) {
+        
         if(c == 'U' && x > 0) {
             if(buf == 'R') x--, y++;
             if(buf == 'L') x--;
+            if(buf == 'U' && x > 2) x-= 2;
+            buf = c;
         } else if(c == 'D' && x < 20) {
             if(buf == 'R') x++, y++;
             if(buf == 'L') x++;
-        } else if(c == 'R' && y < 10) {
+            if(buf == 'D' && x < 19) x+=2;
+            buf = c;
+        } else if(c == 'R' && y < 11) {
             if(buf == 'U') x--;
             if(buf == 'D') x++;
+            if(buf == 'R' && y < 9) y++;
+            buf = c;
         } else if(c == 'L' && y > 0) {
-            if(buf == 'U') x++, y++;
+            if(buf == 'U') x--, y--;
             if(buf == 'D') x++, y--;
-        }
-        
+            if(buf == 'L') y--;
+            buf = c;
+        } 
         if(!visit[x][y]) answer++;
-        cout << c << " (" << x << "," << y << ") " << answer << endl;
         visit[x][y] = 1;
     }
     
