7694a7cc3d92f2025d5747ceb49907530e958bc0
dearyeon <d_earyeon@naver.com>
2022-04-21 22:20:21 +0900

    배달

--- programmers_level2/배달.cpp
+++ programmers_level2/배달.cpp
@@ -18,36 +18,25 @@ int solution(int N, vector<vector<int> > road, int K) {
     sort(road.begin(), road.end());
     vector<int> v(N+1, 0);
     map<int, vector<pair<int, int> > > m;
+    for(int i=0; i<road.size(); i++) {
+        m[road[i][0]].push_back(make_pair(road[i][1], road[i][2]));
+        m[road[i][1]].push_back(make_pair(road[i][0], road[i][2]));
+    }
     queue<int> q;
     q.push(1);
     
 
    while(!q.empty()) {
-       int M = q.front();
-       q.pop();
-       
-        for(int i=0; i<road.size(); i++) {
-            if(road[i][0] != M && road[i][1] != M) continue;
-            
-            int a = M;
-            int b = road[i][0] == M ? road[i][1] : road[i][0];
-            int cmp = v[M] + road[i][2];
-            
-            v[b] = v[b] ? min(v[b],cmp) : cmp;
-            //v[a] = min(v[a],v[b]+road[i][2]);
-            if(v[a] > v[b]+road[i][2]) {
-                v[a] = v[b]+road[i][2];
-                for(int j=0; j<m[a].size(); j++)
-                    v[m[a][i].first] = min(v[m[a][i].first], v[a]+m[a][i].second);
-            }
-            q.push(b);
-            cout << "v["<<a<<"]:"<< v[a] << "  " <<"v["<<b<<"]:"<< v[b] << endl;
-            m[a].push_back(make_pair(b,road[i][2]));
-            m[b].push_back(make_pair(a,road[i][2]));
-            road.erase(road.begin()+i--);
-            
+        int M = q.front();
+        q.pop();
+
+        int r = v[M]; // + road[i][2];
+
+        if(v[M] > r) {
+            v[M] = r;
         }
-       
+        //q.push(b);
+        cout << "v["<<M<<"]:"<< v[M] << endl;
     }
     
     for(int i=1; i<N+1; i++) {
