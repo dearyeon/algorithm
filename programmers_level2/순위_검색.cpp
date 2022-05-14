41af4d94e3467f975325a530d9d1aba4920432de
dearyeon <d_earyeon@naver.com>
2022-05-14 21:37:21 +0900

    순위 검색

--- programmers_level2/순위_검색.cpp
+++ programmers_level2/순위_검색.cpp
@@ -6,46 +6,46 @@
 #include <queue>
 using namespace std;
 
-bool cmp(pair<int, int> &a, pair<int, int> &b) {
-    return a.second > b.second;
+map<string, vector<int> > m;
+map<string, vector<int> >::iterator it;
+
+void putOnMap(vector<string> info) {
+    for(int i=0; i<info.size(); i++) {
+        string cond[4];
+        int pos = 0;
+        
+        for(int i=0; i<3; i++) {
+            int len = info[i].find(" ",pos);
+            cond[i] = info[i].substr(pos, len - pos);
+            pos = len + 5;
+        }
+        cond[3] = info[i].substr(pos, info[i].find(' ',pos)-pos);
+        cout << cond[0] << cond[1] << cond[2] << cond[3] << endl;
+    }
 }
 
 vector<int> solution(vector<string> info, vector<string> query) {
     vector<int> answer;
     
-    map<string, vector<int> > m;
-    map<string, vector<int> >::iterator it;
-    for(int i=0; i<info.size(); i++) {
-        string s = "";
-        int pos = 0;
-        while(info[i].find(' ',pos) != string::npos) {
-            int len = info[i].find(' ',pos);
-            s += info[i].substr(pos, len - pos);
-            pos = len + 1;
-        }
-        m[s].push_back(stoi(info[i].substr(pos)));
-    }
+    putOnMap(info);
     
-    for(it=m.begin(); it!=m.end(); it++)
-        sort(it->second.begin(), it->second.end(), greater<int>());
-    
+    //for(it=m.begin(); it!=m.end(); it++)
+    //    sort(it->second.begin(), it->second.end(), greater<int>());
     for(string q : query) {
-        int isGood = 0;
         int score = stoi(q.substr(q.rfind(' ')));
         string s = "";
-        
+
         vector<string> temp;
         int pos = 0;
         for(int i=0; i<3; i++) {
             int len = q.find(" and ",pos);
-            string word = q.substr(pos, len - pos);
-            if(word == "-") s += to_string(i);
-            else s += word;
+            s += q.substr(pos, len - pos);
             pos = len + 5;
         }
         s += q.substr(pos, q.find(' ',pos)-pos);
-        answer.push_back(isGood);
+        cout << s << endl;
     }
+    
     
     return answer;
 }
